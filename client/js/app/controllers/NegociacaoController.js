368c7c953a8cee641771eec6bdd052d10cd9fc47
Rafael Thayto <rafa.thayto@gmail.com>
2018-02-02 21:44:39 -0200

    Creating JQuery selector

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -4,7 +4,20 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault()
-        alert('teste')
+        
+        // Fazendo o vanilla parecer com JQuery
+        const $ = document.querySelector.bind(document)
+        // ou
+        // const $ = target => document.querySelector(target)
+        const $inputData = $('#data')
+        const $inputQuantidade = $('#quantidade')
+        const $inputValor = $('#valor')
+
+        console.log($inputData.value)
+        console.log($inputQuantidade.value)
+        console.log($inputValor.value)
+        
+
     }
 
 }
