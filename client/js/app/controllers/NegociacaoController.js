6f4596ef1717bfd9cc4cb9c67c622c8066c77288
Rafael Thayto <rafa.thayto@gmail.com>
2018-02-04 18:20:27 -0200

    Refactoring the NegociacaoController

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -9,23 +9,38 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
+        this._listaNegociacoes = new ListaNegociacoes()
         Object.freeze(this)
     }
 
     adiciona(event) {
         event.preventDefault()
+        
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
 
-        const negociacao = new Negociacao(
+        console.log(this._listaNegociacoes.negociacoes)
+        
+        this._limpaFormulario()
+
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         )
+    }
 
-        console.log(negociacao)
+    // O underline na frente é para dizer que esse método só
+    // pode ser chamado pela própria classe
+    _limpaFormulario() {
         
-        console.log(DateHelper.dataParaTexto(negociacao.data))
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0
         
-
+        this._inputData.focus()
     }
 
 }
--- client/js/app/models/ListaNegociacoes.js
+++ client/js/app/models/ListaNegociacoes.js
@@ -1,6 +1,14 @@
 class ListaNegociacoes {
     constructor() {
-
         this._negociacoes= []
-     }
+        Object.freeze(this)
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao)
+    }
+
+    get negociacoes() {
+        return this._negociacoes
+    }
 }
