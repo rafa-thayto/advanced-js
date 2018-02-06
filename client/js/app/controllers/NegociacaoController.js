7ddc7d207c23055a44b903fc88ad296a03b62fe6
Rafael Thayto <rafa.thayto@gmail.com>
2018-02-06 21:09:04 -0200

    Add response message for user

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -10,8 +10,13 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
         this._listaNegociacoes = new ListaNegociacoes()
+        
         this._negociacoesView = new NegociacoesView($('#negociacoesView'))
         this._negociacoesView.update(this._listaNegociacoes)
+        
+        this._mensagem = new Mensagem()
+        this._mensagemView = new MensagemView($('#mensagemView'))
+        this._mensagemView.update(this._mensagem);
         Object.freeze(this)
     }
 
@@ -19,7 +24,10 @@ class NegociacaoController {
         event.preventDefault()
         
         this._listaNegociacoes.adiciona(this._criaNegociacao())
-        this._negociacoesView.update(this._listaNegociacoes)
+        this._negociacoesView.update(this._listaNegociacoes)    
+
+        this._mensagem.texto = 'Negociação adicionada com sucesso!'
+        this._mensagemView.update(this._mensagem);
         this._limpaFormulario()
     }
 
