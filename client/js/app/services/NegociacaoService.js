d8639537c07e80b428bc86d31eb4a8092536a9a4
Rafael Thayto <rafa.thayto@gmail.com>
2018-02-14 21:54:15 -0200

    Creating methods for get negociations in semana passada and retrasada

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -25,4 +25,55 @@ class NegociacaoService {
         xhr.send()
         
     }
+    obterNegociacoesDaSemanaAnterior(callback) {
+        
+        const xhr = new XMLHttpRequest
+
+        // Preparando ambiente
+        xhr.open('GET', 'negociacoes/anterior')
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+
+                    callback(null, JSON.parse(xhr.responseText)
+                        .map(obj => new Negociacao(new Date(obj.data), obj.quantidade, obj.valor)))
+                } else {
+                    console.log(xhr.responseText)
+                    callback('Não foi possível obter as negociações da semana!')
+                }
+            }
+
+        }
+
+        xhr.send()
+        
+    
+    }
+    obterNegociacoesDaSemanaRetrasada(callback) {
+        
+        const xhr = new XMLHttpRequest
+
+        // Preparando ambiente
+        xhr.open('GET', 'negociacoes/retrasada')
+
+        xhr.onreadystatechange = () => {
+
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+
+                    callback(null, JSON.parse(xhr.responseText)
+                        .map(obj => new Negociacao(new Date(obj.data), obj.quantidade, obj.valor)))
+                } else {
+                    console.log(xhr.responseText)
+                    callback('Não foi possível obter as negociações da semana!')
+                }
+            }
+
+        }
+
+        xhr.send()
+        
+    }
 }
